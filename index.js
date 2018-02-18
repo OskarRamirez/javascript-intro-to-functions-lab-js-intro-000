70736295e29ba5388ba13ea597fb5dcc92e059e1
Oskar Ramirez <oskarramirez43@gmail.com>
2018-02-18 22:40:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,4 +21,7 @@ function sayHiToGrandma(string) {
 function sayHiToGrandma(string) {
   if (string.toUpperCase()===string)
   return "YES INDEED!";
+  else if (string==="I love you, Grandma.") {
+    return "I love you, too.";
+  }
 }
